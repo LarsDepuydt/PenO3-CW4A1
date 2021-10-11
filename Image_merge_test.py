f92d2f10943d8bd0a4809a834dcda737f68cb783
RubenJ1 <ruben.janssen3@gmail.com>
2021-10-11 15:33:18 +0200

    Werkende image stitch

--- Image_merge_test.py
+++ Image_merge_test.py
@@ -5,22 +5,16 @@ from matplotlib import pyplot as plt
 from PIL import Image
 
 # Load images
-img1 = cv2.imread("mergeImages/mergePart1.jpg")
-img2 = cv2.imread("mergeImages/mergePart2.jpg")
-# Convert to grayscale
-img1_gray = cv2.cvtColor(img1, cv2.COLOR_BGR2GRAY)
-img2_gray = cv2.cvtColor(img2, cv2.COLOR_BGR2GRAY)
+img1 = cv2.imread("mergeImages/panorama/panorama1.jpg")
+img2 = cv2.imread("mergeImages/panorama/panorama2.jpg")
 
 # Create our ORB detector and detect keypoints and descriptors
 orb = cv2.ORB_create(nfeatures=2000)
 
 # Find the key points and descriptors with ORB
-keypoints1, descriptors1 = orb.detectAndCompute(img1_gray, None)
-keypoints2, descriptors2 = orb.detectAndCompute(img2_gray, None)
+keypoints1, descriptors1 = orb.detectAndCompute(img1, None)
+keypoints2, descriptors2 = orb.detectAndCompute(img2, None)
 
-plt.imshow(cv2.drawKeypoints(img1, keypoints1, None, (255, 0, 255)))
-plt.imshow(cv2.drawKeypoints(img1, keypoints1, None, (255, 0, 255)))
-plt.show()
 
 # Create a BFMatcher object.
 # It will find all of the matching keypoints on two images
@@ -29,34 +23,50 @@ bf = cv2.BFMatcher_create(cv2.NORM_HAMMING)
 # Find matching points
 matches = bf.knnMatch(descriptors1, descriptors2, k=2)
 
-print(keypoints1[0].pt)
-print(keypoints1[0].size)
+# Finding the best matches
+good = []
+for m, n in matches:
+    if m.distance < 0.6 * n.distance:
+        good.append(m)
 
-print("Descriptor of the first keypoint: ")
-print(descriptors1[0])
+def warpImages(img1, img2, H):
+    rows1, cols1 = img1.shape[:2]
+    rows2, cols2 = img2.shape[:2]
 
+    list_of_points_1 = np.float32([[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+    temp_points = np.float32([[0, 0], [0, rows2], [cols2, rows2], [cols2, 0]]).reshape(-1, 1, 2)
 
-def draw_matches(img1, keypoints1, img2, keypoints2, matches):
-    r, c = img1.shape[:2]
-    r1, c1 = img2.shape[:2]
+    # When we have established a homography we need to warp perspective
+    # Change field of view
+    list_of_points_2 = cv2.perspectiveTransform(temp_points, H)
 
-    # Create a blank image with the size of the first image + second image
-    output_img = np.zeros((max([r, r1]), c + c1, 3), dtype='uint8')
-    output_img[:r, :c, :] = np.dstack([img1, img1, img1])
-    output_img[:r1, c:c + c1, :] = np.dstack([img2, img2, img2])
+    list_of_points = np.concatenate((list_of_points_1, list_of_points_2), axis=0)
 
-    # Go over all of the matching points and extract them
-    for match in matches:
-        img1_idx = match.queryIdx
-        img2_idx = match.trainIdx
-        (x1, y1) = keypoints1[img1_idx].pt
-        (x2, y2) = keypoints2[img2_idx].pt
+    [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
+    [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
 
-        # Draw circles on the keypoints
-        cv2.circle(output_img, (int(x1), int(y1)), 4, (0, 255, 255), 1)
-        cv2.circle(output_img, (int(x2) + c, int(y2)), 4, (0, 255, 255), 1)
+    translation_dist = [-x_min, -y_min]
 
-        # Connect the same keypoints
-        cv2.line(output_img, (int(x1), int(y1)), (int(x2) + c, int(y2)), (0, 255, 255), 1)
+    H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
+
+    output_img = cv2.warpPerspective(img2, H_translation.dot(H), (x_max - x_min, y_max - y_min))
+    output_img[translation_dist[1]:rows1 + translation_dist[1], translation_dist[0]:cols1 + translation_dist[0]] = img1
 
     return output_img
+
+
+# Set minimum match condition
+MIN_MATCH_COUNT = 10
+
+if len(good) > MIN_MATCH_COUNT:
+    # Convert keypoints to an argument for findHomography
+    src_pts = np.float32([keypoints1[m.queryIdx].pt for m in good]).reshape(-1, 1, 2)
+    dst_pts = np.float32([keypoints2[m.trainIdx].pt for m in good]).reshape(-1, 1, 2)
+
+    # Establish a homography
+    M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
+
+    result = warpImages(img2, img1, M)
+    cv2.imwrite("test_kleur.jpg", result)
+    plt.imshow(result)
+    plt.show()
