9fe48fbdf258ce5141a4be4d038a0e5f32000be9
Lars Depuydt <lars.depuydt@gmail.com>
2021-10-12 13:28:12 +0200

    tested harris, swift and ORB

--- programs/stitch_images/corner_detection/swift_corner_detection.py
+++ programs/stitch_images/corner_detection/swift_corner_detection.py
@@ -1,9 +1,11 @@
 import cv2 as cv
+import time
+t_start = time.perf_counter()
 
 # Code source: https://docs.opencv.org/master/da/df5/tutorial_py_sift_intro.html
 
 # Read the image
-img = cv.imread('../mergeImages/mergePart1.jpg')
+img = cv.imread('../../../images/testing_images_pi/lokaal/image_for_testing_1.jpg')
 
 # Convert to grayscale
 gray = cv.cvtColor(img, cv.COLOR_BGR2GRAY)
@@ -16,4 +18,6 @@ kp, des = sift.detectAndCompute(gray, None)
 img = cv.drawKeypoints(gray, kp, img, flags=cv.DRAW_MATCHES_FLAGS_DRAW_RICH_KEYPOINTS)
 
 # Save the image
-cv.imwrite('../mergeImages/corners/sift_with_features.jpg', img)
+cv.imwrite('../../../images/testing_images_online/corners/sift_with_features.jpg', img)
+print(time.perf_counter() - t_start)
+print("done")
