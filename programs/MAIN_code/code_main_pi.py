41c8a71a6eb00c16ca72b7a99d60e845bb65fc19
Lars Depuydt <lars.depuydt@gmail.com>
2021-10-21 16:31:45 +0200

    add wrapimages in code_main_pi

--- programs/MAIN_code/code_main_pi.py
+++ programs/MAIN_code/code_main_pi.py
@@ -88,7 +88,7 @@ sender.send_image(RB_MAIN_IP, M)
 
 
 
-
+"""
 def voeg_samen(img_l, img_r, H):
     rows1, cols1 = img_r.shape[:2]
     rows2, cols2 = img_l.shape[:2]
@@ -109,8 +109,34 @@ def voeg_samen(img_l, img_r, H):
     output_img = img_l
     output_img[translation_dist[1]:rows1 + translation_dist[1], translation_dist[0]:cols1 + translation_dist[0]] = img_r
 
-    return output_img
+    return output_img"""
 
+def warpImages(img1, img2, H):
+    print("warp")
+    print(time.process_time())
+    rows1, cols1 = img1.shape[:2]
+    rows2, cols2 = img2.shape[:2]
+
+    list_of_points_1 = np.float32([[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+    temp_points = np.float32([[0, 0], [0, rows2], [cols2, rows2], [cols2, 0]]).reshape(-1, 1, 2)
+
+    # When we have established a homography we need to warp perspective
+    # Change field of view
+    list_of_points_2 = cv2.perspectiveTransform(temp_points, H)
+
+    list_of_points = np.concatenate((list_of_points_1, list_of_points_2), axis=0)
+
+    [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
+    [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
+
+    translation_dist = [-x_min, -y_min]
+
+    H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
+
+    output_img = cv2.warpPerspective(img2, H_translation.dot(H), (x_max - x_min, y_max - y_min))
+    output_img[translation_dist[1]:rows1 + translation_dist[1], translation_dist[0]:cols1 + translation_dist[0]] = img1
+    print(time.process_time())
+    return output_img
 
 #
 # MOET HERHALEN
@@ -124,7 +150,7 @@ imageright = picam.read()
 imageleft = image_hub.recv_image()[1]
 image_hub.send_reply(b'OK')
 
-image2 = voeg_samen(imageleft, imageright, M).read()
+image2 = warpImages(imageleft, imageright, M).read()
 
 '''
 # send to server/host pc
