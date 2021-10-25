daf49d3b6299eb414766d40788f69cdfffba7a6d
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-10-25 17:35:51 +0200

    change ip/networking constants

--- programs/MAIN_code/code_pc.py
+++ programs/MAIN_code/code_pc.py
@@ -1,9 +1,10 @@
 import imagezmq
 import cv2
 
+image_hub = imagezmq.ImageHub()
 # ontvangt foto's
 while True:
-    image_hub = imagezmq.ImageHub()
+
     rpi_name, image = image_hub.recv_image()
     cv2.imshow(rpi_name, image)  # 1 window for each RPi
     cv2.waitKey(1)
