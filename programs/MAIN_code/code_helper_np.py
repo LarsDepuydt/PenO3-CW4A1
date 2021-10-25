bc775567c145141b90ee0a0ac830961679d2a1f4
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-10-25 17:10:11 +0200

    file to test camera quality

--- programs/MAIN_code/code_helper_np.py
+++ programs/MAIN_code/code_helper_np.py
@@ -12,13 +12,16 @@ ETHERNET2 = 'tcp://169.254.165.116:5555'
 RB_IP_HELPER = ETHERNET2
 # RB_IP_MAIN = "tcp://mainraspberry:5555"
 
+RESOLUTION = (1920, 1088)
+
+
 #
 # One time only
 #
 
 # Sends left image
 #cam = PiCamera()
-picam = VideoStream(usePiCamera=True).start()
+picam = VideoStream(usePiCamera=True, resolution=RESOLUTION).start()
 #cam.resolution(640, 480)
 sleep(2.0)  # allow camera sensor to warm up
 imageleft = picam.read()
@@ -39,19 +42,6 @@ output_image = None
 image_list = [left_image, output_image]
 
 
-
-class SendImageToMain(threading.Thread):
-    def __init__(self, lijst):
-        super().__init__()
-        self.lijst = lijst
-
-    def run(self):
-        while True:
-            if self.lijst[1] != None:
-                print("voor error")
-                sender.send_image(RB_IP_MAIN, self.lijst[1])
-
-
 while True:
     # take image
     image_list[0] = picam.read()
@@ -59,24 +49,37 @@ while True:
     #transform image
     print("erin transform")
     rows1, cols1 = image_list[0].shape[:2]
+    print("1")
     list_of_points_1 = np.float32([[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+    print("2")
     temp_points = np.float32([[0, 0], [0, rows1], [cols1, rows1], [cols1, 0]]).reshape(-1, 1, 2)
+    print("3")
 
     # When we have established a homography we need to warp perspective
     # Change field of view
+    print(M)
+    print(temp_points)
     list_of_points_2 = cv2.perspectiveTransform(temp_points, M)
+    print("4")
 
     list_of_points = np.concatenate((list_of_points_1, list_of_points_2), axis=0)
+    print("5")
 
     [x_min, y_min] = np.int32(list_of_points.min(axis=0).ravel() - 0.5)
+    print("6")
     [x_max, y_max] = np.int32(list_of_points.max(axis=0).ravel() + 0.5)
+    print("7")
 
     translation_dist = [-x_min, -y_min]
+    print("8")
 
     H_translation = np.array([[1, 0, translation_dist[0]], [0, 1, translation_dist[1]], [0, 0, 1]])
+    print("9")
 
     image_list[1] = cv2.warpPerspective(image_list[0], H_translation.dot(M), (x_max - x_min, y_max - y_min))
-
+    print("10")
 
     # send output
+    print('send output')
     sender.send_image(RB_IP_MAIN, image_list[1])
+    print('na send output')
