f6acf35679b388e750992a424650a9da6e0a3792
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-10-30 21:35:31 +0200

    bla

--- programs/MAIN_code/non_multi_threaded/test_main.py
+++ programs/MAIN_code/non_multi_threaded/test_main.py
@@ -1,7 +1,5 @@
 from imutils.video import VideoStream
-from picamera import PiCamera
 import imagezmq
-import cv2
 import numpy as np
 from time import sleep
 
@@ -10,8 +8,8 @@ from time import sleep
 # =================
 
 RESOLUTION = (720, 480)
-RB_IP_MAIN = 'tcp://169.254.222.67:5555'
-RB_IP_HELPER = 'tcp://169.254.165.116:5555'
+RB_IP_MAIN = 'tcp://mainraspberry:5555'
+RB_IP_HELPER = 'tcp://helperraspberry:5555'
 
 
 # =================
@@ -23,10 +21,10 @@ sleep(2.0)  # allow camera sensor to warm up
 SENDER = imagezmq.ImageSender(connect_to=RB_IP_HELPER)
 
 # SEND READY MESSAGE
-SENDER.send(np.array(["ready"]))
+SENDER.send_image(RB_IP_MAIN, np.array(["ready"]))
 print("Ready message was received")
 
-left_image = image_hub.recv_image()[1]
+left_image = IMAGE_HUB.recv_image()[1]
 image_hub.send_reply(b'OK')
 print("Received left calibration image")
 
