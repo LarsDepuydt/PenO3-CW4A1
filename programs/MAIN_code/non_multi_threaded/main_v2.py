3da25ac84aec88b1f872ba189dc128aadb9c093d
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-01 23:00:11 +0100

    added troubleshooting.txt, removed old line

--- programs/MAIN_code/non_multi_threaded/main_v2.py
+++ programs/MAIN_code/non_multi_threaded/main_v2.py
@@ -11,7 +11,6 @@ import os
 RESOLUTION = (720, 480)
 RB_IP_MAIN = 'tcp://169.254.222.67:5555'
 RB_IP_HELPER = 'tcp://169.254.165.116:5555'
-PORT = 'tcp://localhost:5556'
 INIT_HELPER_CMD = "sh ssh_login_and_run_file.sh 'cd Desktop/PenO3-CW4A1/programs/MAIN_code/non_multi_threaded;python3 ./test_helper.py'"
 
 # =================
@@ -20,7 +19,7 @@ INIT_HELPER_CMD = "sh ssh_login_and_run_file.sh 'cd Desktop/PenO3-CW4A1/programs
 os.system(INIT_HELPER_CMD)       # init helper pi
 IMAGE_HUB = imagezmq.ImageHub()
 PICAM = VideoStream(usePiCamera=True, resolution=RESOLUTION).start()
-sleep(3.0)  # allow camera sensor to warm up and wait to make sure helper is running
+sleep(4.0)  # allow camera sensor to warm up and wait to make sure helper is running
 SENDER = imagezmq.ImageSender(connect_to=RB_IP_HELPER)
 
 # SEND READY MESSAGE
@@ -32,7 +31,7 @@ IMAGE_HUB.send_reply(b'OK')
 print("Received left calibration image")
 
 
-print("test_main.py END")
+print("main_v2.py ENDED")
 
 
 
