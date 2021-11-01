92d0743c7d98f746b00e907c6d35cbc7465b879e
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-01 22:41:39 +0100

    ssh connection and execute script

--- programs/MAIN_code/non_multi_threaded/main_v2.py
+++ programs/MAIN_code/non_multi_threaded/main_v2.py
@@ -2,6 +2,7 @@ from imutils.video import VideoStream
 import imagezmq
 import numpy as np
 from time import sleep
+import os
 
 # =================
 # CONSTANTS
@@ -10,14 +11,16 @@ from time import sleep
 RESOLUTION = (720, 480)
 RB_IP_MAIN = 'tcp://169.254.222.67:5555'
 RB_IP_HELPER = 'tcp://169.254.165.116:5555'
-
+PORT = 'tcp://localhost:5556'
+INIT_HELPER_CMD = "sh ssh_login_and_run_file.sh 'cd Desktop/PenO3-CW4A1/programs/MAIN_code/non_multi_threaded;python3 ./test_helper.py'"
 
 # =================
 # INITIALIZATION
 # =================
+os.system(INIT_HELPER_CMD)       # init helper pi
 IMAGE_HUB = imagezmq.ImageHub()
 PICAM = VideoStream(usePiCamera=True, resolution=RESOLUTION).start()
-sleep(2.0)  # allow camera sensor to warm up
+sleep(3.0)  # allow camera sensor to warm up and wait to make sure helper is running
 SENDER = imagezmq.ImageSender(connect_to=RB_IP_HELPER)
 
 # SEND READY MESSAGE
@@ -25,11 +28,11 @@ SENDER.send_image(RB_IP_MAIN, np.array(["ready"]))
 print("Ready message was received")
 
 left_image = IMAGE_HUB.recv_image()[1]
-IMAGE_HUBgit .send_reply(b'OK')
+IMAGE_HUB.send_reply(b'OK')
 print("Received left calibration image")
 
 
-print("END")
+print("test_main.py END")
 
 
 
