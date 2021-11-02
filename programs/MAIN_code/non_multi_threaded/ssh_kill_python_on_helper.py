e08095ee80e0ed4d11cd22d8fa8b8ecc614f179d
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-02 14:25:27 +0100

    small changes

--- programs/MAIN_code/non_multi_threaded/ssh_kill_python_on_helper.py
+++ programs/MAIN_code/non_multi_threaded/ssh_kill_python_on_helper.py
@@ -1,6 +1,6 @@
 import os
-
+from time import sleep
 KILL_HELPER_PYTHON_CMD = "sh ssh_conn_and_execute_cmd.sh 'sudo netstat -ltnp; pkill -f helper_v2.py; sudo netstat ltnp'"
-
+sleep(5)
 
 os.system(KILL_HELPER_PYTHON_CMD)       # init helper pi
