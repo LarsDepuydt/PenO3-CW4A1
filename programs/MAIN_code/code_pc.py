c238e719430a739ca9ce702777f1844438180d73
Jasper2701 <jasper.laitem@gmail.com>
2021-11-04 17:47:30 +0100

    nieuwe pogingen helper en main voor straks

--- programs/MAIN_code/code_pc.py
+++ programs/MAIN_code/code_pc.py
@@ -1,11 +1,20 @@
 import imagezmq
 import cv2
+import time
 
-image_hub = imagezmq.ImageHub()
 # ontvangt foto's
+f = []
+t_old = 0
+IMAGE_HUB = imagezmq.ImageHub()
 while True:
-
-    rpi_name, image = image_hub.recv_image()
+    rpi_name, image = IMAGE_HUB.recv_image()
     cv2.imshow(rpi_name, image)  # 1 window for each RPi
     cv2.waitKey(1)
-    image_hub.send_reply(b'OK')
+    IMAGE_HUB.send_reply(b'OK')
+    t = time.perf_counter()
+    f.append(1/(t-t_old))
+    t_old = t
+    print(f)
+print(f)
+
+
