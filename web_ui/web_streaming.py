57eed0d1dc9e9adb91e8ed12afe61dfbfa35a39c
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-15 20:41:42 +0100

    smalls

--- web_ui/web_streaming.py
+++ web_ui/web_streaming.py
@@ -7,6 +7,10 @@ import flask
 # REFERENCE: https://towardsdatascience.com/video-streaming-in-web-browsers-with-opencv-flask-93a38846fe00
 
 MODE = 3
+# 1:  cv2.VideoCapture
+# 2: imutils.VideoStream(usePiCamera=True)
+# 3: imagezmq.ImageHub
+# 4: imagezmq.ImageHub  (log fps)
 
 if MODE == 1:
     camera = cv2.VideoCapture(0)    # laptop webcam
@@ -15,13 +19,13 @@ elif MODE == 2:
     import imutils
     vs = imutils.VideoStream(usePiCamera=True).start() # pi camera
     time.sleep(2.0)
-elif MODE == 3:
-    # DO NOT OPEN IMAGEHUB BEFORE OPENING FLASK!!!!!!!!!!!!
+elif MODE == 3 or MODE == 4:
+    # DO NOT OPEN IMAGEHUB BEFORE app.run'ning flask!
     pass
 
 app = flask.Flask(__name__)
 
-
+# Show camera feed troubleshooting code
 '''
     # Troubleshoot camera: show camera feed
     while True:
@@ -32,9 +36,9 @@ app = flask.Flask(__name__)
             break
     cam.release()
     cv2.destroyAllWindows()
-'''
+    '''
 
-def gen_frames_webcam():
+def gen_frames_cv2_videoCapture():
     frames_per_second, t_old = [], 0
     while True:
         t = time.perf_counter
@@ -53,7 +57,14 @@ def gen_frames_webcam():
             print(sum(frames_per_second)/10)
             frames_per_second = []
 
-def gen_frames_imagehub_with_fps():
+def gen_frames_imagehub():
+    IMAGE_HUB = imagezmq.ImageHub()#open_port='tcp://:5555')
+    while True:
+        frame = IMAGE_HUB.recv_image()[1]
+        IMAGE_HUB.send_reply(b'OK')
+        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n')
+
+def gen_frames_imagehub_log_fps():
     img_hub_fps, web_app_fps, t_old = [], [], 0
     IMAGE_HUB = imagezmq.ImageHub()#open_port='tcp://:5555')
     while True:
@@ -78,26 +89,24 @@ def gen_frames_imagehub_with_fps():
             print("Webserver fps 1sample: ", image_hub_fps[-1])
             webserver_fps, image_hub_fps = [], []
 
-def gen_frames_imagehub():
-    IMAGE_HUB = imagezmq.ImageHub()#open_port='tcp://:5555')
-    while True:
-        frame = IMAGE_HUB.recv_image()[1]
-        IMAGE_HUB.send_reply(b'OK')
-        yield(b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + cv2.imencode('.jpg', frame)[1].tobytes() + b'\r\n')
-
-
 @app.route('/')
 def index():
     return flask.render_template('index.html')
 
-if MODE == 1 or MODE == 2:
+if MODE == 1:
     @app.route('/video_feed')
     def video_feed():
-        return flask.Response(gen_frames_webcam(), mimetype='multipart/x-mixed-replace; boundary=frame')
+        return flask.Response(gen_frames_cv2_videoCapture(), mimetype='multipart/x-mixed-replace; boundary=frame')
+elif MODE == 2:
+    pass
 elif MODE == 3:
     @app.route('/video_feed')
     def video_feed():
         return flask.Response(gen_frames_imagehub(), mimetype='multipart/x-mixed-replace; boundary=frame')
+elif MODE == 4:
+    @app.route('/video_feed')
+    def video_feed():
+        return flask.Response(gen_frames_imagehub_log_fps(), mimetype='multipart/x-mixed-replace; boundary=frame')
 
 if __name__ == "__main__":
     app.run(debug=True)
