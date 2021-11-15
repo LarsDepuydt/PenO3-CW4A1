921a493a5173d25723cd4dd32c5a5b6f5f585c04
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-15 14:30:43 +0100

    dunno

--- programs/MAIN_code/code_pc.py
+++ programs/MAIN_code/code_pc.py
@@ -7,5 +7,8 @@ while True:
 
     rpi_name, image = image_hub.recv_image()
     cv2.imshow(rpi_name, image)  # 1 window for each RPi
-    cv2.waitKey(1)
     image_hub.send_reply(b'OK')
+    if cv2.waitKey(1) == 27:
+        cv2.destroyAllWindows
+        break
+    print('here')
