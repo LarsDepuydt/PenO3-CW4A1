e35eded4d83e3486d275a720aca979db4d0d6794
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-11-19 13:52:07 +0100

    bla

--- programs/MAIN_code/non_multi_threaded/one_dim_image_matcher_v0.py
+++ programs/MAIN_code/non_multi_threaded/one_dim_image_matcher_v0.py
@@ -151,10 +151,3 @@ cv2.waitKey(0)
 cv2.imwrite('./outputimg.png', imgOut)
 
 '''
-# Convert keypoints to an argument for findHomography
-src_pts = np.float32([keyptsL[m.queryIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-dst_pts = np.float32([keyptsR[m.trainIdx].pt for m in good_matches]).reshape(-1, 1, 2)
-
-# Establish a homography
-M, _ = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC, 5.0)
-
