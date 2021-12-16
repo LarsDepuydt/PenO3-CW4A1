8c5675c440bff5316053e6a43a5984f9a98f33d0
TheOnlyRealEngineer <robin.anne@student.kuleuven.be>
2021-12-16 23:56:53 +0100

    testing no helper wait

--- programs/code_pc.py
+++ programs/code_pc.py
@@ -6,9 +6,10 @@ from time import perf_counter
 t_old = 0
 fps = []
 IMAGE_HUB = imagezmq.ImageHub()
+img = cv2.imread("C:/Users/robin/Documents/School/2021_2022/PnO III/PenO3-CW4A1/images/web_interface_image.jpg")
 while True:
     rpi_name, image = IMAGE_HUB.recv_image()
-    cv2.imshow(rpi_name, image)  # 1 window for each RPi
+    cv2.imshow(rpi_name, img)  # 1 window for each RPi
     cv2.waitKey(1)
     IMAGE_HUB.send_reply(b'OK')
     t = perf_counter()
